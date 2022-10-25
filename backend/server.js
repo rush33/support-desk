b615e007ad5a4a0f93b03b5b05730981842916fa
Rushad <rushadmahmed@gmail.com>
2022-10-25 11:33:09 +0530

    initial commit

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,25 @@
 const express = require("express");
+const colors = require("colors");
+const {errorHandler} = require('./controller/errorHandler'); 
+const connectDB = require('./config/db')
 const dotenv = require("dotenv").config();
 const PORT = process.env.PORT || 8000;
 
+//Connect to database
+connectDB()
+
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.get("/", (req, res) => {
   res.json({ message: "Welcome to Support Desk API" });
 });
 
 //Routes
-app.use("/api/users/", require("./routes/userRoutes"));
+app.use("/api/users", require("./routes/userRoutes"));
 
-app.listen(PORT, () => console.log(`Server started on port {PORT}`));
+app.use(errorHandler)
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
